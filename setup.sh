2d05d6d9d82c4a5a51fb873cb4fe306a5cdd75d3
KuoE0 <kuoe0.tw@gmail.com>
2016-05-26 14:32:28 +0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# setup.sh
+# KuoE0 <kuoe0.tw@gmail.com>
+# Copyright (C) 2016
+#
+# Distributed under terms of the MIT license.
+#
+
+TMUX_FOLDER="$HOME/.tmux"
+TMUX_CONFIG="$HOME/.tmux.conf"
+PLUGIN_FOLDER="./plugins"
+
+if [ -d "$PLUGIN_FOLDER" ]; then
+	rm -rf "$PLUGIN_FOLDER"
+fi
+mkdir "$PLUGIN_FOLDER"
+
+git clone --depth 1 https://github.com/tmux-plugins/tpm plugins/tpm
+
+if [ -f "$TMUX_FOLDER" ]; then
+	rm "$TMUX_FOLDER"
+fi
+ln -s "$PWD" "$TMUX_FOLDER"
+
+if [ -f "$TMUX_CONFIG" ]; then
+	rm "$TMUX_CONFIG"
+fi
+ln -s "$PWD/tmux.conf" "$TMUX_CONFIG"
+
+echo "Launch tmux and run <prefix+I> to install plugins..."
